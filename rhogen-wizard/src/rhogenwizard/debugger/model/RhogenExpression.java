f796d2c249a6a238adf455ae0ecfa793a83926f0
anton vishnevskiy <antonv@rhomobile.com>
2011-05-05 20:26:44 +0400

    added expression class

---
+++ rhogen-wizard/src/rhogenwizard/debugger/model/RhogenExpression.java
@@ -0,0 +1,68 @@
+package rhogenwizard.debugger.model;
+
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.IExpression;
+import org.eclipse.debug.core.model.IValue;
+
+import rhogenwizard.debugger.RhogenConstants;
+
+public class RhogenExpression implements IExpression
+{
+	IDebugTarget m_target;
+	ILaunch      m_launch;
+	String       m_expName;
+	IValue       m_expValue;
+	
+	RhogenExpression(IDebugTarget target, ILaunch launch, String name, IValue value)
+	{
+		m_target = target;
+		m_launch = launch;
+		m_expName  = name;
+		m_expValue = value;
+	}
+
+	@Override
+	public String getModelIdentifier() 
+	{
+		return RhogenConstants.debugModelId;
+	}
+
+	@Override
+	public ILaunch getLaunch() 
+	{
+		return m_launch;
+	}
+
+	@Override
+	public Object getAdapter(Class adapter) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getExpressionText() 
+	{
+		return m_expName;
+	}
+
+	@Override
+	public IValue getValue() 
+	{
+		return m_expValue;
+	}
+
+	@Override
+	public IDebugTarget getDebugTarget() 
+	{
+		return m_target;
+	}
+
+	@Override
+	public void dispose()
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
