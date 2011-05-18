45e1366706631f44e1573a8aeb24ff5ad09e375d
Albert R. Timashev <albert@timashev.com>
2011-05-18 15:08:24 +0400

    Debug Server: improved step/suspend/breakpoint callbacks

--- rhogen-wizard/src/rhogenwizard/debugger/DebugState.java
+++ rhogen-wizard/src/rhogenwizard/debugger/DebugState.java
@@ -28,11 +28,62 @@ public enum DebugState {
 	 */
 	BREAKPOINT,
 	/**
-	 * Rhodes application is stopped after 'step' command.
+	 * Rhodes application is stopped after 'step into' command.
 	 */
-	STEP,
+	STOPPED_INTO,
+	/**
+	 * Rhodes application is stopped after 'step over' command.
+	 */
+	STOPPED_OVER,
+	/**
+	 * Rhodes application is stopped after 'step return' command.
+	 */
+	STOPPED_RETURN,
+	/**
+	 * Rhodes application is stopped after 'suspend' command.
+	 */
+	SUSPENDED,
 	/**
 	 * Rhodes application is exited (terminated).
 	 */
-	EXITED
+	EXITED;
+
+	/**
+	 * Check the Rhodes application debug status--is it paused or not.
+	 * @param state - specified debug state.
+	 * @return Returns <code>true</code> if execution of the application is suspended
+	 * by stop at breakpoint, 'step' command or 'suspend' command.
+	 * Returns <code>false</code> otherwise.  
+	 */
+	public static boolean paused(DebugState state) {
+		return (state==DebugState.BREAKPOINT) || (state==DebugState.STOPPED_INTO) || (state==DebugState.STOPPED_OVER) || (state==DebugState.STOPPED_RETURN) || (state==DebugState.SUSPENDED);
+	}
+
+	/**
+	 * Get debug state name by ({@link DebugState}).
+	 * @param state - debug state ({@link DebugState}).
+	 * @return Returns a {@link String} representing a name of the debug state: 
+	 * <code>"disconnected"</code> for {@link DebugState#NOTCONNECTED},
+	 * <code>"connected"</code> for {@link DebugState#CONNECTED},
+	 * <code>"runnung"</code> for {@link DebugState#RUNNING},
+	 * <code>"breakpoint"</code> for {@link DebugState#BREAKPOINT},
+	 * <code>"stopped (into)"</code> for {@link DebugState#STOPPED_INTO},
+	 * <code>"stopped (over)"</code> for {@link DebugState#STOPPED_OVER},
+	 * <code>"stopped (return)"</code> for {@link DebugState#STOPPED_RETURN},
+	 * <code>"suspended"</code> for {@link DebugState#SUSPENDED},
+	 * <code>"terminated"</code> for {@link DebugState#EXITED}.
+	 */
+	public static String getName(DebugState state) {
+		switch (state) {
+		case NOTCONNECTED: return "disconnected";
+		case CONNECTED: return "connected";
+		case RUNNING: return "runnung";
+		case BREAKPOINT: return "breakpoint";
+		case STOPPED_INTO: return "stopped (into)";
+		case STOPPED_OVER: return "stopped (over)";
+		case STOPPED_RETURN: return "stopped (return)";
+		case SUSPENDED: return "suspended";
+		default: return "terminated";
+		}
+	}
 }
