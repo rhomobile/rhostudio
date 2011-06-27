5c9a90770b1c2135b12a2833091b6ca9389ec45e
anton vishnevskiy <antonv@rhomobile.com>
2011-06-27 22:30:30 +0400

    uncomment check rhodes versio

--- rhogen-wizard/src/rhogenwizard/wizards/RhogenAppWizard.java
+++ rhogen-wizard/src/rhogenwizard/wizards/RhogenAppWizard.java
@@ -120,13 +120,13 @@ public class RhogenAppWizard extends Wizard implements INewWizard
 			
 			newProject = RhodesProjectSupport.createProject(infoHolder);
 
-//			if (!checkRhodesVersion(CommonConstants.rhodesVersion))
-//			{
-//				newProject.delete(true, monitor);
-//				ShowMessageJob msgJob = new ShowMessageJob("", "Error", "Installed Rhodes have old version. Please reinstall it (See 'http://docs.rhomobile.com/rhodes/install' for more information)");
-//				msgJob.run(monitor);
-//				return;
-//			}
+			if (!checkRhodesVersion(CommonConstants.rhodesVersion))
+			{
+				newProject.delete(true, monitor);
+				ShowMessageJob msgJob = new ShowMessageJob("", "Error", "Installed Rhodes have old version. Please reinstall it (See 'http://docs.rhomobile.com/rhodes/install' for more information)");
+				msgJob.run(monitor);
+				return;
+			}
 			
 			if (!infoHolder.existCreate) 
 			{
