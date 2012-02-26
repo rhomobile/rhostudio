e48ef4d529b6d0fdeeae34094ea0682951c4fbfa
Anton Vishnevski <antonv@rhomobile.com>
2012-02-26 04:57:22 +0400

    remove white space

--- rhogen-wizard/src/rhogenwizard/OSValidator.java
+++ rhogen-wizard/src/rhogenwizard/OSValidator.java
@@ -14,36 +14,33 @@ public class OSValidator
 	{
 		if(isWindows()){
 			return OSType.WINDOWS;
-		}else if(isMac()){
-			return OSType.MACOS;
-		}else if(isUnix()){
+		} else if(isMac()){
+		 	return OSType.MACOS;
+		} else if(isUnix()){
 			return OSType.UNIXLIKE;
 		}
 		
 		return OSType.UNDEFINED;
 	}
  
-	public static boolean isWindows(){
- 
+	public static boolean isWindows()
+	{ 
 		String os = System.getProperty("os.name").toLowerCase();
 		//windows
-	    return (os.indexOf( "win" ) >= 0); 
- 
+	    return (os.indexOf( "win" ) >= 0);  
 	}
  
-	public static boolean isMac(){
- 
+	public static boolean isMac()
+	{ 
 		String os = System.getProperty("os.name").toLowerCase();
 		//Mac
-	    return (os.indexOf( "mac" ) >= 0); 
- 
+	    return (os.indexOf( "mac" ) >= 0);  
 	}
  
-	public static boolean isUnix(){
- 
+	public static boolean isUnix()
+	{ 
 		String os = System.getProperty("os.name").toLowerCase();
 		//linux or unix
-	    return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
- 
+	    return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0); 
 	}
 }
