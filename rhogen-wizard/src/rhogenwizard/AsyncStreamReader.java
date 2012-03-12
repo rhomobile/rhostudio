ea22a9a704c73cfa34300bcb721cfb5cbf622ac1
Anton Vishnevski <antonv@rhomobile.com>
2012-03-13 03:05:05 +0400

    fix code policy

--- rhogen-wizard/src/rhogenwizard/AsyncStreamReader.java
+++ rhogen-wizard/src/rhogenwizard/AsyncStreamReader.java
@@ -1,43 +1,42 @@
 package rhogenwizard;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class AsyncStreamReader extends Thread
 {
-	private StringBuffer fBuffer = null;
-	private InputStream  fInputStream = null;
-	private String       fThreadId = null;
-	private boolean      fStop = false;
-	private boolean      fReadFile = false;
-	private ILogDevice   fLogDevice = null;
+	private StringBuffer m_buffer = null;
+	private InputStream  m_inputStream = null;
+	private String       m_threadId = null;
+	private boolean      m_stop = false;
+	private boolean      m_readFile = false;
+	private ILogDevice   m_logDevice = null;
 	
 	private String fNewLine = null;
 	
 	public AsyncStreamReader(boolean readFile, InputStream inputStream, StringBuffer buffer, ILogDevice logDevice, String threadId)
 	{
-		fReadFile = readFile;
-		fInputStream = inputStream;
-		fBuffer = buffer;
-		fThreadId = threadId;
-		fLogDevice = logDevice;
+		m_readFile    = readFile;
+		m_inputStream = inputStream;
+		m_buffer      = buffer;
+		m_threadId    = threadId;
+		m_logDevice   = logDevice;
 		
 		fNewLine = System.getProperty("line.separator");
 	}	
 	
-	public String getBuffer() {		
-		return fBuffer.toString();
+	public String getBuffer()
+	{		
+		return m_buffer.toString();
 	}
 	
 	public void run()
 	{
 		try 
 		{
-			if (fReadFile)
+			if (m_readFile)
 			{
 				readFile();
 			}
@@ -54,16 +53,16 @@ public class AsyncStreamReader extends Thread
 	
 	private void readFile() throws IOException, InterruptedException
 	{
-		BufferedReader bufOut = new BufferedReader(new InputStreamReader(fInputStream));
+		BufferedReader bufOut = new BufferedReader(new InputStreamReader(m_inputStream));
 		String line = null;
 		
-		while (fStop == false)
+		while (m_stop == false)
 		{
 			if (bufOut.ready())
 			{
 				line = bufOut.readLine();
 				
-				fBuffer.append(line + fNewLine);
+				m_buffer.append(line + fNewLine);
 				printToDisplayDevice(line);				
 			}
 			else
@@ -73,34 +72,34 @@ public class AsyncStreamReader extends Thread
 		}
 		
 		bufOut.close();
-		printToConsole("END OF: " + fThreadId); //DEBUG
+		printToConsole("END OF: " + m_threadId); //DEBUG
 	}
 	
 	private void readCommandOutput() throws IOException, InterruptedException
 	{		
-		BufferedReader bufOut = new BufferedReader(new InputStreamReader(fInputStream));		
+		BufferedReader bufOut = new BufferedReader(new InputStreamReader(m_inputStream));		
 		String line = null;
 		
-		while ( (fStop == false) && ((line = bufOut.readLine()) != null) )
+		while ( (m_stop == false) && ((line = bufOut.readLine()) != null) )
 		{
-			fBuffer.append( line + fNewLine);
+			m_buffer.append( line + fNewLine);
 			printToDisplayDevice(line);				
 		}		
 
 		bufOut.close();
-		printToConsole("END OF: " + fThreadId); //DEBUG
+		printToConsole("END OF: " + m_threadId); //DEBUG
 	}
 	
 	public void stopReading() 
 	{
-		printToConsole("call stoping " + fThreadId); //DEBUG
-		fStop = true;
+		printToConsole("call stoping " + m_threadId); //DEBUG
+		m_stop = true;
 	}
 	
 	private void printToDisplayDevice(String line)
 	{
-		if( fLogDevice != null )
-			fLogDevice.log(line);
+		if( m_logDevice != null )
+			m_logDevice.log(line);
 		else
 		{
 			printToConsole(line);//DEBUG
