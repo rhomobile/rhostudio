d509d073964cff701b7bebd21cb2aa7bec70b0db
Anton Vishnevski <antonv@rhomobile.com>
2012-03-21 19:39:08 +0400

    perspective refactoring

--- rhogen-wizard/src/rhogenwizard/perspectives/RhostudioPerspective.java
+++ rhogen-wizard/src/rhogenwizard/perspectives/RhostudioPerspective.java
@@ -7,7 +7,11 @@ import org.eclipse.ui.console.IConsoleConstants;
 
 public class RhostudioPerspective implements IPerspectiveFactory 
 {
-	public static final String PERSPECTIVE_ID = "rhogenwizard.perspectives.RhostudioPerspective";
+	public static final String idEGitRepositaryView = "org.eclipse.egit.ui.RepositoriesView";
+	public static final String idEGitActopnSet = "org.eclipse.egit.ui.gitaction";
+	public static final String idDltkRubyPerspective = "org.eclipse.dltk.ruby.ui.RubyPerspective";
+    public static final String idDltkRubyBrowsePerspective = "org.eclipse.dltk.ruby.ui.RubyBrowsingPerspective";
+	public static final String idPerspective = "rhogenwizard.perspectives.RhostudioPerspective";
 	
 	private IPageLayout m_factory;
 
@@ -24,7 +28,6 @@ public class RhostudioPerspective implements IPerspectiveFactory
 		addActionSets();
 		addNewWizardShortcuts();
 		addPerspectiveShortcuts();
-		addViewShortcuts();
 	}
 
 	private void addViews() 
@@ -33,6 +36,7 @@ public class RhostudioPerspective implements IPerspectiveFactory
 		
 		bottom.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 		bottom.addView(IPageLayout.ID_PROGRESS_VIEW);
+		bottom.addView(idEGitRepositaryView);
 
 		IFolderLayout topLeft = m_factory.createFolder("topLeft", IPageLayout.LEFT, 0.25f, m_factory.getEditorArea());
 		
@@ -44,12 +48,13 @@ public class RhostudioPerspective implements IPerspectiveFactory
 		m_factory.addActionSet("org.eclipse.debug.ui.launchActionSet"); //NON-NLS-1
 		m_factory.addActionSet("org.eclipse.debug.ui.debugActionSet"); //NON-NLS-1
 		m_factory.addActionSet("org.eclipse.debug.ui.profileActionSet"); //NON-NLS-1
+		m_factory.addActionSet(idEGitActopnSet); //NON-NLS-1
 	}
 
 	private void addPerspectiveShortcuts()
 	{
-		m_factory.addPerspectiveShortcut("org.eclipse.team.ui.TeamSynchronizingPerspective"); //NON-NLS-1
-		m_factory.addPerspectiveShortcut("org.eclipse.team.cvs.ui.cvsPerspective"); //NON-NLS-1
+		m_factory.addPerspectiveShortcut(idDltkRubyPerspective); //NON-NLS-1
+		m_factory.addPerspectiveShortcut(idDltkRubyBrowsePerspective); //NON-NLS-1
 	}
 
 	private void addNewWizardShortcuts() 
@@ -59,19 +64,5 @@ public class RhostudioPerspective implements IPerspectiveFactory
 		m_factory.addNewWizardShortcut("rhogenwizard.wizards.ModelWizard");//NON-NLS-1
 		m_factory.addNewWizardShortcut("rhogenwizard.wizards.ExtensionWizard");//NON-NLS-1
 		m_factory.addNewWizardShortcut("rhogenwizard.wizards.SourceAdapterWizard");//NON-NLS-1
-	}
-
-	private void addViewShortcuts() 
-	{
-//		factory.addShowViewShortcut("org.eclipse.ant.ui.views.AntView"); //NON-NLS-1
-//		factory.addShowViewShortcut("org.eclipse.team.ccvs.ui.AnnotateView"); //NON-NLS-1
-//		factory.addShowViewShortcut("org.eclipse.pde.ui.DependenciesView"); //NON-NLS-1
-//		factory.addShowViewShortcut("org.eclipse.jdt.junit.ResultView"); //NON-NLS-1
-//		factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); //NON-NLS-1
-//		factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-//		factory.addShowViewShortcut(JavaUI.ID_PACKAGES);
-//		factory.addShowViewShortcut(IPageLayout.ID_RES_NAV);
-//		factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-//		factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
 	}
 }
