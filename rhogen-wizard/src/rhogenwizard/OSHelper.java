95d14eee0c71ed942f2b69905d7a18b3c49d446b
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-04-21 01:26:01 +0400

    OSHelper.concat is fixed (bug in Windows)

--- rhogen-wizard/src/rhogenwizard/OSHelper.java
+++ rhogen-wizard/src/rhogenwizard/OSHelper.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public class OSHelper extends OSValidator
 {
-	public static void killScriptProcess(String processName) throws Exception
-	{
-		killProcess(processName, processName + ".bat"); 
-	}
-	
+    public static void killScriptProcess(String processName) throws Exception
+    {
+        killProcess(processName, processName + ".bat"); 
+    }
+    
     public static void killProcess(int pid) throws Exception
     {
         List<String> cmdLine;
@@ -26,62 +26,65 @@ public class OSHelper extends OSValidator
         new SysCommandExecutor().runCommand(cmdLine);
     }
     
-	public static void killProcess(String processName) throws Exception
-	{
-		killProcess(processName, processName + ".exe"); 
-	}
-	
-	public static void killProcess(String unixName, String wndName) throws Exception
-	{
-		List<String> cmdLine = new ArrayList<String>();
-		
-		if (OSValidator.OSType.WINDOWS == OSValidator.detect()) 
-		{
-			cmdLine.add("taskkill.exe");
-			cmdLine.add("/F");
-			cmdLine.add("/IM");
-			cmdLine.add(wndName);
-		}
-		else
-		{
-			cmdLine.add("killall");
-			cmdLine.add("-9");
-			cmdLine.add(unixName);
-		}
-		
-		SysCommandExecutor executor = new SysCommandExecutor();
-		executor.runCommand(cmdLine);
-	}
-	
-	public static void deleteFolder(String pathToRootFolder)
-	{
-		File rootFolder = new File(pathToRootFolder);
-		
-		deleteFolder(rootFolder);
-	}
-	
-	public static void deleteFolder(File rootFolder)
-	{
-		if (!rootFolder.isDirectory())
-		{
-			rootFolder.delete();
-			return;
-		}
-		
-		File[] containFiles = rootFolder.listFiles();
-		
-		for (File currFile : containFiles)
-		{
-			deleteFolder(currFile);
-		}
-		
-		rootFolder.delete();
-	}
-
-	public static File concat(String... paths)
+    public static void killProcess(String processName) throws Exception
     {
-        File file = new File("");
-        for (int i = 0; i < paths.length; i++)
+        killProcess(processName, processName + ".exe"); 
+    }
+    
+    public static void killProcess(String unixName, String wndName) throws Exception
+    {
+        List<String> cmdLine = new ArrayList<String>();
+        
+        if (OSValidator.OSType.WINDOWS == OSValidator.detect()) 
+        {
+            cmdLine.add("taskkill.exe");
+            cmdLine.add("/F");
+            cmdLine.add("/IM");
+            cmdLine.add(wndName);
+        }
+        else
+        {
+            cmdLine.add("killall");
+            cmdLine.add("-9");
+            cmdLine.add(unixName);
+        }
+        
+        SysCommandExecutor executor = new SysCommandExecutor();
+        executor.runCommand(cmdLine);
+    }
+    
+    public static void deleteFolder(String pathToRootFolder)
+    {
+        File rootFolder = new File(pathToRootFolder);
+        
+        deleteFolder(rootFolder);
+    }
+    
+    public static void deleteFolder(File rootFolder)
+    {
+        if (!rootFolder.isDirectory())
+        {
+            rootFolder.delete();
+            return;
+        }
+        
+        File[] containFiles = rootFolder.listFiles();
+        
+        for (File currFile : containFiles)
+        {
+            deleteFolder(currFile);
+        }
+        
+        rootFolder.delete();
+    }
+
+    public static File concat(String... paths)
+    {
+        // under Windows concatination starting from [new File("")]
+        // leads to path starting with [\]
+        
+        File file = new File((paths.length <= 0) ? "" : paths[0]);
+        for (int i = 1; i < paths.length; i++)
         {
             file = new File(file, paths[i]);
         }
