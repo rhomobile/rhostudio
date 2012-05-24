f5177978f333678fc11d9ecedb2b1a8032e2410d
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-05-24 20:24:54 +0400

    BuildPlatformTask.java, SelectPlatformBuildJob.java: cleaning before editing

--- rhogen-wizard/src/rhogenwizard/builder/rhodes/SelectPlatformBuildJob.java
+++ rhogen-wizard/src/rhogenwizard/builder/rhodes/SelectPlatformBuildJob.java
@@ -7,44 +7,40 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+
 import rhogenwizard.PlatformType;
 import rhogenwizard.sdk.facade.RhoTaskHolder;
 import rhogenwizard.sdk.task.BuildPlatformTask;
 
-public class SelectPlatformBuildJob extends Job 
+public class SelectPlatformBuildJob extends Job
 {
-	private PlatformType m_selectPlatform = PlatformType.eUnknown;
-	private String       m_workDir = null;
-	
-	public SelectPlatformBuildJob(String name, String workDir, PlatformType plType) 
-	{
-		super(name);
-		
-		m_workDir = workDir;
-		m_selectPlatform = plType;
-	}
-	
-    @Override
-	protected void canceling() 
+    private PlatformType m_selectPlatform = PlatformType.eUnknown;
+    private String m_workDir = null;
+
+    public SelectPlatformBuildJob(String name, String workDir, PlatformType plType)
     {
-    	RhoTaskHolder.getInstance().stopTask(BuildPlatformTask.class);
-		super.canceling();
-	}
+        super(name);
 
-	public PlatformType getSelectedPlatform()
-	{
-		return m_selectPlatform;
-	}
+        m_workDir = workDir;
+        m_selectPlatform = plType;
+    }
 
-	@Override
-	protected IStatus run(IProgressMonitor monitor) 
-	{
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put(BuildPlatformTask.workDir, m_workDir);
-		params.put(BuildPlatformTask.platformType, m_selectPlatform);
+    @Override
+    protected void canceling()
+    {
+        RhoTaskHolder.getInstance().stopTask(BuildPlatformTask.class);
+        super.canceling();
+    }
 
-		RhoTaskHolder.getInstance().runTask(BuildPlatformTask.class, params);
+    @Override
+    protected IStatus run(IProgressMonitor monitor)
+    {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put(BuildPlatformTask.workDir, m_workDir);
+        params.put(BuildPlatformTask.platformType, m_selectPlatform);
 
-		return Status.OK_STATUS; 
-	}
+        RhoTaskHolder.getInstance().runTask(BuildPlatformTask.class, params);
+
+        return Status.OK_STATUS;
+    }
 }
--- rhogen-wizard/src/rhogenwizard/sdk/task/BuildPlatformTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/BuildPlatformTask.java
@@ -7,25 +7,27 @@ import rhogenwizard.sdk.helper.TaskResultConverter;
 
 public class BuildPlatformTask extends RakeTask
 {
-	public static final String platformType = "platform-type";
-	
-	@Override
-	public void run() 
-	{
-		try 
-		{
-			if (m_taskParams == null || m_taskParams.size() == 0)
-				throw new InvalidAttributesException("parameters data is invalid [BuildPlatformTask]");
-		
-			String       workDir      = (String) m_taskParams.get(IRunTask.workDir);
-			PlatformType platformType = (PlatformType) m_taskParams.get(CleanPlatformTask.platformType);
+    public static final String platformType = "platform-type";
 
-			runRakeTask(workDir, "device:" + platformType.toString() + ":production");
-		}
-		catch (Exception e) 
-		{
-			Integer resCode = new Integer(TaskResultConverter.failCode);  
-			m_taskResult.put(resTag, resCode);
-		}	
-	}
+    @Override
+    public void run()
+    {
+        try
+        {
+            if (m_taskParams == null || m_taskParams.size() == 0)
+                throw new InvalidAttributesException(
+                        "parameters data is invalid [BuildPlatformTask]");
+
+            String workDir = (String) m_taskParams.get(IRunTask.workDir);
+            PlatformType platformType =
+                    (PlatformType) m_taskParams.get(CleanPlatformTask.platformType);
+
+            runRakeTask(workDir, "device:" + platformType.toString() + ":production");
+        }
+        catch (Exception e)
+        {
+            Integer resCode = new Integer(TaskResultConverter.failCode);
+            m_taskResult.put(resTag, resCode);
+        }
+    }
 }
