3e896b5bea20987ca74429793fa1e0a11e4cdc88
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-05-24 18:59:40 +0400

    GenerateRhoconnectAdapterTask.java, rhoconnect/SourceAdapterWizard.java: cleaning before editing

--- rhogen-wizard/src/rhogenwizard/sdk/task/GenerateRhoconnectAdapterTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/GenerateRhoconnectAdapterTask.java
@@ -2,7 +2,6 @@ package rhogenwizard.sdk.task;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.naming.directory.InvalidAttributesException;
 
@@ -10,37 +9,39 @@ import rhogenwizard.sdk.helper.TaskResultConverter;
 
 public class GenerateRhoconnectAdapterTask extends RhoconnectTask
 {
-	public static final String sourceName = "source-name";
+    public static final String sourceName = "source-name";
 
-	@Override
-	public void run() 
-	{
-		m_taskResult.clear();
-		
-		try 
-		{			
-			if (m_taskParams == null || m_taskParams.size() == 0)
-				throw new InvalidAttributesException("parameters data is invalid [GenerateRhoconnectAdapterTask]");		
-			
-			String workDir    = (String) m_taskParams.get(IRunTask.workDir);
-			String sourceName = (String) m_taskParams.get(GenerateRhoconnectAdapterTask.sourceName);
-								
-			m_executor.setWorkingDirectory(workDir);
-			
-			List<String> cmdLine = new ArrayList<String>();
-			cmdLine.add(m_rhoConnectExe);
-			cmdLine.add("source");
-			cmdLine.add(sourceName);
-			
-			int res = m_executor.runCommand(cmdLine);
-			
-			Integer resCode = new Integer(res);			
-			m_taskResult.put(resTag, resCode);		
-		}
-		catch (Exception e) 
-		{
-			Integer resCode = new Integer(TaskResultConverter.failCode);  
-			m_taskResult.put(resTag, resCode);		
-		}
-	}
+    @Override
+    public void run()
+    {
+        m_taskResult.clear();
+
+        try
+        {
+            if (m_taskParams == null || m_taskParams.size() == 0)
+                throw new InvalidAttributesException(
+                        "parameters data is invalid [GenerateRhoconnectAdapterTask]");
+
+            String workDir = (String) m_taskParams.get(IRunTask.workDir);
+            String sourceName =
+                    (String) m_taskParams.get(GenerateRhoconnectAdapterTask.sourceName);
+
+            m_executor.setWorkingDirectory(workDir);
+
+            List<String> cmdLine = new ArrayList<String>();
+            cmdLine.add(m_rhoConnectExe);
+            cmdLine.add("source");
+            cmdLine.add(sourceName);
+
+            int res = m_executor.runCommand(cmdLine);
+
+            Integer resCode = new Integer(res);
+            m_taskResult.put(resTag, resCode);
+        }
+        catch (Exception e)
+        {
+            Integer resCode = new Integer(TaskResultConverter.failCode);
+            m_taskResult.put(resTag, resCode);
+        }
+    }
 }
