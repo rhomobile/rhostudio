c5d788e494fe6df27e859dca339dd06f76318fa5
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-05-26 19:07:25 +0400

    SeqRunTask is added

---
+++ rhogen-wizard/src/rhogenwizard/sdk/task/SeqRunTask.java
@@ -0,0 +1,46 @@
+package rhogenwizard.sdk.task;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class SeqRunTask extends RunTask
+{
+    private final RunTask m_tasks[];
+
+    public SeqRunTask(RunTask... tasks)
+    {
+        m_tasks = tasks;
+    }
+
+    @Override
+    public void setData(Map<String, ?> data)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void run(IProgressMonitor monitor)
+    {
+        for (RunTask task : m_tasks)
+        {
+            if (monitor.isCanceled())
+            {
+                throw new StoppedException();
+            }
+
+            task.run(monitor);
+        }
+    }
+
+    @Override
+    public Map<String, ?> getResult()
+    {
+        if (m_tasks.length == 0)
+        {
+            return new HashMap<String, Object>();
+        }
+        return m_tasks[m_tasks.length - 1].getResult();
+    }
+}
