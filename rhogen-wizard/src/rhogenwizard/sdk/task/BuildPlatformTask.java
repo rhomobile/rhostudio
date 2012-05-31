a429c466537a7c0e8bc349f4b36347258e62401f
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-05-31 20:45:11 +0400

    Production build action shows build console

--- rhogen-wizard/src/rhogenwizard/sdk/task/BuildPlatformTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/BuildPlatformTask.java
@@ -1,5 +1,6 @@
 package rhogenwizard.sdk.task;
 
+import rhogenwizard.ConsoleHelper;
 import rhogenwizard.PlatformType;
 
 public class BuildPlatformTask extends ARubyTask
@@ -7,5 +8,12 @@ public class BuildPlatformTask extends ARubyTask
     public BuildPlatformTask(String workDir, PlatformType platformType)
     {
         super(workDir, "rake", "device:" + platformType + ":production");
+    }
+
+    @Override
+    protected void exec()
+    {
+        ConsoleHelper.showBuildConsole();
+        super.exec();
     }
 }
