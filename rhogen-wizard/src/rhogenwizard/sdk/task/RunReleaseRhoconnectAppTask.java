b8f9e01d536bb9c6f6bd26186c18fb73a66f85f7
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-05-31 20:39:22 +0400

    RunReleaseRhoconnectAppTask.java: 'rake rhoconnect:start' command is replaced with 'rake rhoconnect:startbg'

--- rhogen-wizard/src/rhogenwizard/sdk/task/RunReleaseRhoconnectAppTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/RunReleaseRhoconnectAppTask.java
@@ -1,40 +1,14 @@
 package rhogenwizard.sdk.task;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-
-import rhogenwizard.ConsoleHelper;
-
 public class RunReleaseRhoconnectAppTask extends SeqRunTask
 {
     private static RunTask[] getTasks(final String workDir)
     {
         RunTask redisStartbgTask = new ARubyTask(workDir, "rake", "redis:startbg");
-
-        RunTask rhoconnectStartTask = new RunTask()
-        {
-            @Override
-            public boolean isOk()
-            {
-                return true;
-            }
-
-            @Override
-            public void run(IProgressMonitor monitor)
-            {
-                new Thread(new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        ConsoleHelper.showAppConsole();
-                        new ARubyTask(workDir, "rake", "rhoconnect:start").run();
-                    }
-                }).start();
-            }
-        };
+        RunTask rhoconnectStartbgTask = new ARubyTask(workDir, "rake", "rhoconnect:startbg");
 
         return new RunTask[] { new StopSyncAppTask(), new StoreLastSyncRunAppTask(workDir), redisStartbgTask,
-            rhoconnectStartTask };
+            rhoconnectStartbgTask };
     }
 
     public RunReleaseRhoconnectAppTask(String workDir)
