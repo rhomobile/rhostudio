4c87c36fe22e449783da9d8171e90949b86e1b5a
Anton Vishnevski <antonv@rhomobile.com>
2012-06-03 22:03:01 +0400

    added egit default repo path set

--- rhogen-wizard/src/rhogenwizard/Activator.java
+++ rhogen-wizard/src/rhogenwizard/Activator.java
@@ -1,10 +1,16 @@
 package rhogenwizard;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -14,72 +20,81 @@ import rhogenwizard.sdk.task.StopSyncAppTask;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin
+{
+    // The plug-in ID
+    public static final String PLUGIN_ID = "RhogenWizard";        //$NON-NLS-1$
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "RhogenWizard"; //$NON-NLS-1$
+    // The shared instance
+    private static Activator plugin;
 
-	// The shared instance
-	private static Activator plugin;
-	
     private Set<Integer> runningProcessesIdsForRunReleaseRhodesAppTask = new HashSet<Integer>();
 
-	/**
-	 * The constructor
-	 */
-	public Activator() 
-	{
-	}
+    /**
+     * The constructor
+     */
+    public Activator()
+    {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception 
-	{
-		super.start(context);
-		plugin = this;
-		
-		ConsoleHelper.consoleAppPrint(" ");
-		ConsoleHelper.consoleBuildPrint(" ");
-		ConsoleHelper.showBuildConsole();
-	}
+    private void setEgitDefaultRepositaryPath()
+    {
+        // get object which represents the workspace
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception 
-	{
-	    killProcessesForForRunReleaseRhodesAppTask();
+        // get location of workspace (java.io.File)
+        File workspaceDirectory = workspace.getRoot().getLocation().toFile();
 
-	    new StopSyncAppTask().run();
+        IEclipsePreferences instanceNode = new InstanceScope().getNode("org.eclipse.egit.ui");
+        instanceNode.put(UIPreferences.DEFAULT_REPO_DIR, workspaceDirectory.toString());
+    }
 
-		plugin = null;
-		super.stop(context);		
-	}
+    @SuppressWarnings("deprecation")
+    public void start(BundleContext context) throws Exception
+    {
+        super.start(context);
+        plugin = this;
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() 
-	{
-		return plugin;
-	}
+        // set our default 
+        setEgitDefaultRepositaryPath();
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) 
-	{
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+        ConsoleHelper.consoleAppPrint(" ");
+        ConsoleHelper.consoleBuildPrint(" ");
+        ConsoleHelper.showBuildConsole();
+    }
+
+    public void stop(BundleContext context) throws Exception
+    {
+        killProcessesForForRunReleaseRhodesAppTask();
+
+        new StopSyncAppTask().run();
+
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault()
+    {
+        return plugin;
+    }
+
+    /**
+     * Returns an image descriptor for the image file at the given plug-in
+     * relative path
+     * 
+     * @param path
+     *            the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path)
+    {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 
     public void storeProcessesForForRunReleaseRhodesAppTask(Set<Integer> pids)
     {
@@ -125,13 +140,15 @@ public class Activator extends AbstractUIPlugin {
         throw new ActivatorException(e);
     }
 
-    // for severity use IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR, IStatus.CANCEL
+    // for severity use IStatus.OK, IStatus.INFO, IStatus.WARNING,
+    // IStatus.ERROR, IStatus.CANCEL
     private static void log(int severity, String msg)
     {
         plugin.getLog().log(new Status(severity, PLUGIN_ID, msg));
     }
 
-    // for severity use IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR, IStatus.CANCEL
+    // for severity use IStatus.OK, IStatus.INFO, IStatus.WARNING,
+    // IStatus.ERROR, IStatus.CANCEL
     private static void log(int severity, String msg, Throwable e)
     {
         plugin.getLog().log(new Status(severity, PLUGIN_ID, msg, e));
