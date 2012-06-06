af949703057327a3b9bc1c12ad0169d69deb0023
Anton Vishnevski <antonv@rhomobile.com>
2012-06-06 22:44:01 +0400

    fix issues for windows platform

--- rhogen-wizard/src/rhogenwizard/sdk/task/RubyCodeExecTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/RubyCodeExecTask.java
@@ -7,18 +7,23 @@ public class RubyCodeExecTask extends RubyExecTask
 {
     public RubyCodeExecTask(String... codeLines)
     {
-        super(null, "ruby", getArgs(codeLines));
+        super("c:\\Android", "ruby", getArgs(codeLines));
+        
         disableConsole();
     }
 
     private static String[] getArgs(String[] codeLines)
     {
         List<String> args = new ArrayList<String>();
+        
         for (String codeLine : codeLines)
         {
+            codeLine = codeLine.replaceAll("\\\"", "\\\\\"");
+            
             args.add("-e");
-            args.add(codeLine);
+            args.add("\"" + codeLine + "\"");
         }
+        
         return args.toArray(new String[0]);
     }
 }
