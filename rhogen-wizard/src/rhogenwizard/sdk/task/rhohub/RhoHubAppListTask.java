a6dc299929872b10a83d9ac2ad63bfc7532faa23
Anton Vishnevski <antonv@rhomobile.com>
2012-06-14 19:29:08 +0400

    add rhohub tasks

---
+++ rhogen-wizard/src/rhogenwizard/sdk/task/rhohub/RhoHubAppListTask.java
@@ -0,0 +1,35 @@
+package rhogenwizard.sdk.task.rhohub;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import rhogenwizard.sdk.task.RubyCodeExecTask;
+
+public class RhoHubAppListTask extends RubyCodeExecTask
+{
+    public RhoHubAppListTask(String userToken, String serverUrl)
+    {
+        super("require 'rhohub'", 
+              "Rhohub.token = \"" + userToken + "\"", 
+              "Rhohub.url = \"" + serverUrl + "\"", 
+              "puts Rhohub::App.list()");
+    }
+
+    public JSONArray getOutputAsJSON() throws JSONException
+    {
+        String listOfApps = this.getOutput();
+        
+        listOfApps = listOfApps.replaceAll("\\p{Cntrl}", " ");
+        
+        // TODO - its temporary solutions, wait Lucas for fix api output 
+        String[] a = listOfApps.split("\\[");
+        
+        if (a.length < 2)
+            return null;
+        
+        String[] b = a[1].split("\\]");
+        // end of
+               
+        return new JSONArray("[" + b[0] + "]");
+    }
+}
---
+++ rhogen-wizard/src/rhogenwizard/sdk/task/rhohub/RhoHubBuildApp.java
@@ -0,0 +1,8 @@
+package rhogenwizard.sdk.task.rhohub;
+
+import rhogenwizard.sdk.task.RubyCodeExecTask;
+
+public class RhoHubBuildApp extends RubyCodeExecTask
+{
+
+}
