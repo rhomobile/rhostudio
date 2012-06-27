a616222a770032dfc2cca446bd2bec8250bd6dba
Anton Vishnevski <antonv@rhomobile.com>
2012-06-28 01:42:05 +0400

    add question dialog

--- rhogen-wizard/src/rhogenwizard/DialogUtils.java
+++ rhogen-wizard/src/rhogenwizard/DialogUtils.java
@@ -44,4 +44,20 @@ public class DialogUtils
 
         return ok[0];
     }
+    
+    public static boolean quetsion(final String title, final String message)
+    {
+        final boolean ok[] = new boolean[1];
+
+        PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                ok[0] = MessageDialog.openQuestion(null, title, message);
+            }
+        });
+
+        return ok[0];
+    }
 }
