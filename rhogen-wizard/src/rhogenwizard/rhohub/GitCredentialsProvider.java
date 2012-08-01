139765e5d0c219862ead9463ffadd62375c89d92
Anton Vishnevski <antonv@rhomobile.com>
2012-08-02 01:28:57 +0400

    fix cancelling git pwd dialog

--- rhogen-wizard/src/rhogenwizard/rhohub/GitCredentialsProvider.java
+++ rhogen-wizard/src/rhogenwizard/rhohub/GitCredentialsProvider.java
@@ -1,11 +1,13 @@
 package rhogenwizard.rhohub;
 
 import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.errors.UnsupportedCredentialItem;
 import org.eclipse.jgit.transport.CredentialItem;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.widgets.Display;
 
 import rhogenwizard.DialogUtils;
@@ -27,26 +29,23 @@ public class GitCredentialsProvider extends CredentialsProvider
 	    	{
 	    		public void run() 
 	    		{
-	    			try 
-	    			{
-				        InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(), 
-				            "Git password", "Please enter you git password.", "", null) 
-				        {
-				            @Override
-				            protected int getInputTextStyle() 
-				            {
-				                return SWT.SINGLE | SWT.BORDER | SWT.PASSWORD;
-				            }
-				        };
-				        dlg.open();
-				        
-				        CredentialItem.StringType pwdCred = (CredentialItem.StringType)arg1[0];
-				        pwdCred.setValue(dlg.getValue());
-	    			} 
-	    			catch (Exception ex) 
-	    			{
-	    				ex.printStackTrace();
-	    			}
+			        InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(), 
+			            "Git password", "Please enter you git password:", "", null) 
+			        {
+			            @Override
+			            protected int getInputTextStyle() 
+			            {
+			                return SWT.SINGLE | SWT.BORDER | SWT.PASSWORD;
+			            }
+			        };
+			        
+			        if (dlg.open() == Window.CANCEL)
+			        {
+			        	throw new SWTException(SWT.ERROR_FAILED_EXEC);
+			        }
+			        
+			        CredentialItem.StringType pwdCred = (CredentialItem.StringType)arg1[0];
+			        pwdCred.setValue(dlg.getValue() == null ? "" : dlg.getValue());
 	    		}
 	    	});
     	}
