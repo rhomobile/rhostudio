d321d2fa00ed28736b92264889a9ebc74d652904
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-08-08 16:27:45 +0400

    'rhoconnect startbg' in separated console

--- rhogen-wizard/src/rhogenwizard/sdk/task/run/RunReleaseRhoconnectAppTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/run/RunReleaseRhoconnectAppTask.java
@@ -1,5 +1,6 @@
 package rhogenwizard.sdk.task.run;
 
+import rhogenwizard.OSHelper;
 import rhogenwizard.SysCommandExecutor;
 import rhogenwizard.sdk.task.RubyExecTask;
 import rhogenwizard.sdk.task.RunTask;
@@ -13,8 +14,17 @@ public class RunReleaseRhoconnectAppTask extends SeqRunTask
     {
         RunTask redisStartbgTask = new RubyExecTask(workDir, SysCommandExecutor.RUBY_BAT, "rhoconnect",
             "redis-startbg");
-        RunTask rhoconnectStartbgTask = new RubyExecTask(workDir, SysCommandExecutor.RUBY_BAT, "rhoconnect",
-            "startbg");
+        RunTask rhoconnectStartbgTask;
+        if (OSHelper.isWindows())
+        {
+            rhoconnectStartbgTask = new RubyExecTask(workDir, SysCommandExecutor.RUBY_BAT, "start",
+                "rhoconnect", "start");
+        }
+        else
+        {
+            rhoconnectStartbgTask = new RubyExecTask(workDir, SysCommandExecutor.RUBY_BAT, "osascript", "-e",
+                "tell app \"Terminal\"\ndo script \"cd " + workDir + "&&rhoconnect start&&exit\"\nend tell");
+        }
 
         return new RunTask[] { new StopSyncAppTask(), new StoreLastSyncRunAppTask(workDir), redisStartbgTask,
             rhoconnectStartbgTask };
