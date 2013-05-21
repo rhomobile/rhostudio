b93770f7d164f8a2aff40a47662819d6d74f4356
Ramadev <Ramdev.cdac@gmail.com>
2013-05-21 15:22:06 +0530

    handle capability dialog crash

--- rhogen-wizard/src/rhogenwizard/editors/Capabilities.java
+++ rhogen-wizard/src/rhogenwizard/editors/Capabilities.java
@@ -2,101 +2,104 @@ package rhogenwizard.editors;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import rhogenwizard.PlatformType;
 
-public enum Capabilities
-{
-	eUnknown(null, null),
-	eGps("gps", PlatformType.eUnknown),
-	ePim("pim", PlatformType.eUnknown),
-	eCamera("camera", PlatformType.eUnknown),
-	eVibrate("vibrate", PlatformType.eUnknown),
-	ePhone("phone", PlatformType.eUnknown),
-	eBluetooth("bluetooth", PlatformType.eUnknown),
-	eCalendar("calendar", PlatformType.eUnknown),
-	eNoMotoDevice("non_motorola_device", PlatformType.eUnknown),
-	eNativeBrowser("native_browser", PlatformType.eUnknown),
-	eMotoBrowser("motorola_browser", PlatformType.eUnknown),
-	eHardAccelerate("hardware_acceleration", PlatformType.eAndroid),
-        eNetWorkState("network_state",PlatformType.eAndroid),
-	eSDCard("sdcard",PlatformType.eAndroid);
-	
-	public final String       publicId;
-	public final PlatformType platformId;
+public class Capabilities {
 
-	private Capabilities(final String publicName, final PlatformType platform)
-	{
-		platformId = platform;
-		publicId   = publicName;
-	}
-	
-    public static String[] getPublicIds()
-    {
+    private static ArrayList<Capabilities> capabilityList = new ArrayList<Capabilities>();
+
+    public String publicId;
+    public PlatformType platformId;
+
+    private Capabilities(String publicName, PlatformType platform) {
+        platformId = platform;
+        publicId = publicName;
+    }
+
+    public static String[] getPublicIds() {
         return getPublicIdsList().toArray(new String[0]);
     }
-    
-    public static List<String> getPublicIdsList()
-    {
+
+    public static List<String> getPublicIdsList() {
         List<String> list = new ArrayList<String>();
-        
-        for (Capabilities pt : Capabilities.values())
-        {
-            if (pt.publicId != null)
-            {
+
+        for (Capabilities capability : capabilityList) {
+            if (capability.publicId != null) {
+                list.add(capability.publicId);
+            }
+        }
+        return list;
+    }
+
+    public static List<String> getPublicIdsList(List<Capabilities> capabList) {
+        List<String> list = new ArrayList<String>();
+
+        for (Capabilities pt : capabList) {
+            if (pt.publicId != null) {
                 list.add(pt.publicId);
             }
         }
-        
         return list;
     }
-    
-    public static List<String> getPublicIdsList(List<Capabilities> capabList)
-    {
-        List<String> list = new ArrayList<String>();
-        
-        for (Capabilities pt : capabList)
-        {
-            if (pt.publicId != null)
-            {
-                list.add(pt.publicId);
-            }
-        }
-        
-        return list;
-    }
-    
-    public static List<Capabilities> getCapabilitiesList(List<String> capabList)
-    {
+
+    public static List<Capabilities> getCapabilitiesList(List<String> capabList) {
         List<Capabilities> list = new ArrayList<Capabilities>();
-        
-        if (capabList != null)
-        {
-	        for (String pt : capabList)
-	        {
-	            list.add(Capabilities.fromId(pt));
-	        }
-        }
-        
-        return list;
-    }
-    
-    public static Capabilities fromId(String id)
-    {
-        for (Capabilities pt : Capabilities.values())
-        {
-            if (id.equals(pt.publicId))
-            {
-                return pt;
+        defaultCapabilityList();
+        if (capabList != null) {
+            for (String pt : capabList) {
+                list.add(Capabilities.fromId(pt));
             }
         }
-        
-        return Capabilities.eUnknown;
+        return list;
     }
-    
+
+    // Create default device capability list
+    public static void defaultCapabilityList() {
+
+        if (capabilityList.size() > 0)
+            capabilityList.clear();
+
+        capabilityList.add(new Capabilities("gps", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("pim", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("camera", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("vibrate", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("phone", PlatformType.eUnknown));
+        capabilityList
+                .add(new Capabilities("bluetooth", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("calendar", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("non_motorola_device",
+                PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("native_browser",
+                PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("motorola_browser",
+                PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("hardware_acceleration",
+                PlatformType.eAndroid));
+        capabilityList.add(new Capabilities("push", PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("network_state",
+                PlatformType.eUnknown));
+        capabilityList.add(new Capabilities("sdcard", PlatformType.eUnknown));
+
+    }
+
+    public static Capabilities fromId(String id) {
+        for (Capabilities capability : capabilityList) {
+            if (id.equals(capability.publicId)) {
+                return capability;
+            }
+        }
+
+        // this is new capability, add it to the list and show it in capability
+        // dialog
+        Capabilities UnknownCapability = new Capabilities(id,
+                PlatformType.eUnknown);
+        capabilityList.add(UnknownCapability);
+        return UnknownCapability;
+    }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return publicId;
     }
-};
+
+}
