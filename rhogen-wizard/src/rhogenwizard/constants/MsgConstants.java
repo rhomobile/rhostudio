a8f72e47de2fc04a0e5b486821851b7f81061cae
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-01-20 13:41:44 +0300

    rhogenwizard.launcher.rhoelements.LaunchDelegate class was removed.

--- rhogen-wizard/src/rhogenwizard/constants/MsgConstants.java
+++ rhogen-wizard/src/rhogenwizard/constants/MsgConstants.java
@@ -12,9 +12,6 @@ public class MsgConstants
 	
 	public static final String titleRhoelementsPage = "RhoElements application generator wizard";
 	
-	public static final String rhoelementsWarrningLicense = "Before run the application as RhoElements application see and agree RhoElements " +
-			"licence (http://edgedocs.rhomobile.com/rhoelements/moto-api-beta4#licensing). If you press OK button application type in build.yml will be changed to rhoelements";
-	
 	public static final String preferencesPageTitle = "RhoMobile rhobuild.yml preferences";
 	
 	public static final String preferencesRhoHubTitle = "RhoHub settings preferences";
