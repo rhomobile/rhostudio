aacc0a20d5a641f7c328258d469ed477c352143f
anton vishnevski <antonv@rhomobile.com>
2015-01-30 00:20:10 +0300

    [live update] async ui, first stage

--- rhogen-wizard/src/rhogenwizard/actions/LiveUpdateDiscoverAction.java
+++ rhogen-wizard/src/rhogenwizard/actions/LiveUpdateDiscoverAction.java
@@ -1,12 +1,19 @@
 package rhogenwizard.actions;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import rhogenwizard.DialogUtils;
 import rhogenwizard.builder.rhodes.ConfigProductionBuildDialog;
+import rhogenwizard.project.ProjectFactory;
+import rhogenwizard.project.RhodesProject;
+import rhogenwizard.project.RhoelementsProject;
+import rhogenwizard.rhohub.TokenChecker;
 
 public class LiveUpdateDiscoverAction implements IWorkbenchWindowActionDelegate 
 {
@@ -15,9 +22,27 @@ public class LiveUpdateDiscoverAction implements IWorkbenchWindowActionDelegate
 	@Override
 	public void run(IAction action) 
 	{
-        ConfigProductionBuildDialog dialog = new ConfigProductionBuildDialog(window.getShell());
+		IProject project = ProjectFactory.getInstance().getSelectedProject();
+
+        if (project == null)
+        {
+            DialogUtils.error("Error", "Before run live update discover select RhoMobile project's");
+            return;
+        }
+
+        if (!RhodesProject.checkNature(project) && !RhoelementsProject.checkNature(project))
+        {
+            DialogUtils.error("Error", "Live update feature can run only for RhoMobile project's");
+            return;
+        }
+
+        if (!TokenChecker.processToken(project))
+            return;
+        
+        LiveUpdateDiscoverDialog dialog = new LiveUpdateDiscoverDialog(project, window.getShell());
+        
         dialog.create();
-         
+          
         if (dialog.open() == Window.OK)
         {
         	
@@ -27,15 +52,11 @@ public class LiveUpdateDiscoverAction implements IWorkbenchWindowActionDelegate
 	@Override
 	public void selectionChanged(IAction action, ISelection selection)
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void dispose() 
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
