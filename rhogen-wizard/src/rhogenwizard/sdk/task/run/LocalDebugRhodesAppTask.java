cdda260c62f579971cc1df0e2b3b18b6a501ad9c
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2015-04-17 18:12:28 +0300

    rhogen-wizard/src/rhogenwizard/sdk/task/run/LocalDebugRhodesAppTask.java: use new debug commands.

--- rhogen-wizard/src/rhogenwizard/sdk/task/run/LocalDebugRhodesAppTask.java
+++ rhogen-wizard/src/rhogenwizard/sdk/task/run/LocalDebugRhodesAppTask.java
@@ -1,9 +1,5 @@
 package rhogenwizard.sdk.task.run;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,22 +18,80 @@ import rhogenwizard.sdk.task.SeqRunTask;
 
 public class LocalDebugRhodesAppTask implements IDebugTask
 {
+    private static interface IArgsBuilder
+    {
+        String[] getArgs(String stage);
+    }
+
+    private static IArgsBuilder makeArgsBuilder(final PlatformType platformType,
+            final RunType runType, final boolean reloadCode, final boolean trace,
+            final String startPathOverride, final String[] additionalRubyExtensions)
+    {
+        return new IArgsBuilder()
+        {
+            @Override
+            public String[] getArgs(String stage)
+            {
+                List<String> args = new ArrayList<String>();
+                args.add("rake");
+
+                if (runType == RunType.eRhoSimulator)
+                {
+                    args.add(join(":", "run", platformType.toString(), "rhosimulator", stage));
+                }
+                else if(runType == RunType.eSimulator)
+                {
+                    // for emulator
+                    args.add(join(":", "run", platformType.toString(), stage));
+                    args.add("rho_remote_debug=true");
+                }
+                else if(runType == RunType.eDevice)
+                {
+                    // for device
+                    args.add(join(":", "run", platformType.toString(), "device", stage));
+                    args.add("rho_remote_debug=true");
+                }
+                else
+                {
+                    return null;
+                }
+
+                if (trace)
+                {
+                    args.add("--trace");
+                }
+
+                args.add("rho_debug_port=9000");
+                args.add("rho_reload_app_changes=" + (reloadCode ? "1" : "0"));
+
+                if (startPathOverride != null)
+                {
+                    args.add("rho_override_start_path=\'" + startPathOverride + "\'");
+                }
+
+                if (additionalRubyExtensions != null && additionalRubyExtensions.length > 0)
+                {
+                    args.add("rho_extensions=" + join(",", additionalRubyExtensions));
+                }
+
+                return args.toArray(new String[0]);
+            }
+        };
+    }
+
     private final IDebugTask m_debugTask;
     private final SeqRunTask m_seqTask;
 
     public LocalDebugRhodesAppTask(ILaunch launch, RunType runType, String workDir,
         String appName, PlatformType platformType, boolean isReloadCode, boolean isTrace,
         String startPathOverride, String[] additionalRubyExtensions)
-            throws IOException
     {
-        File debugCommandFile = File.createTempFile("debug_command_", null);
-        debugCommandFile.deleteOnExit();
-        String dcf = debugCommandFile.getPath();
-
+        IArgsBuilder ab = makeArgsBuilder(platformType, runType, isTrace, isReloadCode,
+                startPathOverride, additionalRubyExtensions);
         IRunTask buildTask = new RubyExecTask(workDir, SysCommandExecutor.RUBY_BAT,
-            getBuildArgs(platformType, runType, isTrace, isReloadCode, startPathOverride,
-                additionalRubyExtensions, dcf));
-        m_debugTask = getDebugTask(launch, workDir, appName, dcf);
+            ab.getArgs("build"));
+        m_debugTask = new RubyDebugTask(launch, appName, workDir, SysCommandExecutor.RUBY_BAT,
+            ab.getArgs("debug"));
 
         m_seqTask = new SeqRunTask(buildTask, m_debugTask);
     }
@@ -64,130 +118,6 @@ public class LocalDebugRhodesAppTask implements IDebugTask
     public IProcess getDebugProcess()
     {
         return m_debugTask.getDebugProcess();
-    }
-
-    private static String[] getBuildArgs(PlatformType platformType, RunType runType,
-        boolean isReloadCode, boolean isTrace, String startPathOverride,
-        String[] additionalRubyExtensions, String debugCommandFile)
-    {
-        List<String> args = new ArrayList<String>();
-        args.add("rake");
-        
-        if (runType == RunType.eRhoSimulator)
-        {
-            args.add("run:" + platformType + ":rhosimulator_debug[" + debugCommandFile + "]");
-        }
-        else if(runType == RunType.eSimulator)
-        {
-        	// for emulator
-            args.add("run:" + platformType + "[" + debugCommandFile + "]");
-            args.add("rho_remote_debug=true");
-        }
-        else if(runType == RunType.eDevice)
-        {
-        	// for device
-            args.add("run:" + platformType + ":device" + "[" + debugCommandFile + "]");
-            args.add("rho_remote_debug=true");
-        }
-        else
-        {
-        	return null;
-        }
-
-        if (isTrace)
-        {
-            args.add("--trace");
-        }
-
-        args.add("rho_debug_port=9000");
-        args.add("rho_reload_app_changes=" + (isReloadCode ? "1" : "0"));
-        
-        if (startPathOverride != null)
-        {
-            args.add("rho_override_start_path=\'" + startPathOverride + "\'");
-        }
-
-        if (additionalRubyExtensions != null && additionalRubyExtensions.length > 0)
-        {
-            args.add("rho_extensions=" + join(",", additionalRubyExtensions));
-        }
-        
-        return args.toArray(new String[0]);
-    }
-
-    private static IDebugTask getDebugTask(final ILaunch launch, final String workDir,
-        final String appName, final String debugCommandFile)
-    {
-        return new IDebugTask()
-        {
-            private IDebugTask m_task = null;
-
-            @Override
-            public void run()
-            {
-                init().run();
-            }
-
-            @Override
-            public void run(IProgressMonitor monitor)
-            {
-                init().run(monitor);
-            }
-
-            @Override
-            public boolean isOk()
-            {
-                return get().isOk();
-            }
-
-            @Override
-            public IProcess getDebugProcess()
-            {
-                return get().getDebugProcess();
-            }
-
-            private IDebugTask init()
-            {
-                String[] args;
-                try
-                {
-                    args = readLinesFromFile(debugCommandFile);
-                }
-                catch (IOException e)
-                {
-                    throw new RuntimeException(e);
-                }
-                m_task = new RubyDebugTask(launch, appName, workDir, SysCommandExecutor.CRT, args);
-                return m_task;
-            }
-
-            private IDebugTask get()
-            {
-                if (m_task == null)
-                {
-                    throw new IllegalStateException("The task is not finished yet.");
-                }
-                return m_task;
-            }
-        };
-    }
-
-    private static String[] readLinesFromFile(String path) throws IOException
-    {
-        List<String> lines = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(path)))
-        {
-            while (true)
-            {
-                String line = br.readLine();
-                if (line == null)
-                {
-                    break;
-                }
-                lines.add(line);
-            }
-        }
-        return lines.toArray(new String[0]);
     }
 
     private static String join(String delimiter, String... text)
